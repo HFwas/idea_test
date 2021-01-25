7b188709f509678288e1864c76f2f2eb7858d4a2
HFwas <1558934404@qq.com>
2021-01-25 23:23:56 +0800

    "2021-1-25:idea从GitHub拉取代码实验"

---
+++ test12/src/Demo01.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: HFwas
+ * @Date: 2021/1/25
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("IDEA拉去GitHub仓库代码实验");
+    }
+}
